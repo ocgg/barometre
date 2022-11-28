4ad41d8f8bc011d376b8f790ba62f59b7c47a649
Olivier Genel <zotri@hotmail.com>
2022-11-28 15:06:02 +0100

    controllers created

---
+++ test/controllers/preferences_controller_test.rb
@@ -0,0 +1,33 @@
+require "test_helper"
+
+class PreferencesControllerTest < ActionDispatch::IntegrationTest
+  test "should get show" do
+    get preferences_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get preferences_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get preferences_create_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get preferences_edit_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get preferences_update_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get preferences_destroy_url
+    assert_response :success
+  end
+end
