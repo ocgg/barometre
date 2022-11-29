dd973ef74f181e0a9ead40747cfb36e4ee7773a0
Pierre Quignon <p.quignon.pro@gmail.com>
2022-11-29 15:55:51 +0100

    debut code controller stimulus

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -4,8 +4,23 @@ class BookmarksController < ApplicationController
   end
 
   def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.user = current_user
+    @event = Event.find(params[:event_id])
+    @bookmark.event = @event
+    if @bookmark.save
+      redirect_to
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:bookmark_id)
   end
 end
