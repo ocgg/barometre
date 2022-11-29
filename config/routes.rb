08c807a4acf3f1f9966ab83f30938bf1da8396da
Pierre Quignon <p.quignon.pro@gmail.com>
2022-11-29 14:59:58 +0100

    modification des routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   root to: "pages#home"
 
-  resources :events, only: %i[index show new create]
-  resources :bookmarks, only: %i[index show create destroy]
+  resources :events, only: %i[index show new create] do
+    resources :bookmarks, only: %i[create]
+  end
+  resources :bookmarks, only: %i[index destroy]
   resources :preferences, only: %i[show new create edit update destroy]
 end
