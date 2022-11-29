785c6036945024f0815303d82535de13be9dea31
Olivier Genel <zotri@hotmail.com>
2022-11-29 11:10:37 +0100

    fixed validations and has_many through relations to events and subcategories

--- app/models/subcategory.rb
+++ app/models/subcategory.rb
@@ -1,7 +1,7 @@
 class Subcategory < ApplicationRecord
   belongs_to :category
   has_many :preferences
-  # has_many :events
+  has_many :events, through: :tags
   has_many :tags
 
   validates :name, presence: true
