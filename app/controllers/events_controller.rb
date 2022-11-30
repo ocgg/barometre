3ece570fac4fe9cefd7827c093452ca78ef6c915
Olivier Genel <zotri@hotmail.com>
2022-11-30 15:57:29 +0100

    search bar initialized

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,7 @@
 class EventsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
   def index
-    @events = Event.all
+    @events = set_events
     @bookmarks = current_user.bookmarks if user_signed_in?
     @bookmark = Bookmark.new
   end
@@ -24,7 +24,21 @@ class EventsController < ApplicationController
     end
   end
 
+  private
+
   def event_params
     params.require(:event).permit(:name, :date, :description, :venue)
   end #cette méthode devra etre adaptée au projet, elle est nécessaire dans le setup de cloudinary (Pierre)
+
+  def set_events
+    if params[:query].present?
+      sql_query = <<~SQL
+        events.name @@ :query OR events.description @@ :query
+        OR venues.name @@ :query OR venues.description @@ :query
+      SQL
+      Event.joins(:venue).where(sql_query, query: "%#{params[:query]}%")
+    else
+      Event.all
+    end
+  end
 end
