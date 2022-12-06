2d157a5505032ca919de60eecce2e2a2c051b921
Olivier Genel <zotri@hotmail.com>
2022-12-06 12:21:42 +0100

    plein de trucs

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -8,17 +8,30 @@ class VenuesController < ApplicationController
   end
 
   def create
-    redirect_to events_path
+    @venue = Venue.new(venue_params)
+    unless @venue.photo.present?
+      @venue.photo.attach(io: File.open('app/assets/images/microbw.png'), filename: 'microbw.png', content_type: 'image/png')
+    end
+    if @venue.save!
+      redirect_to new_venue_event_path(@venue)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
+
+  def venue_params
+    params.require(:venue).permit(:name, :address, :description, :photo)
+  end
 
   def set_venues
     # Si il y a une requete dans la search bar,
     if params[:query].present?
       # filtrer les events avec cette requête SQL
       sql_query = <<~SQL
-        venues.name @@ :query OR venues.address @@ :query
+        venues.name ILIKE :query
+        OR venues.address ILIKE :query
       SQL
       Venue.where(sql_query, query: "%#{params[:query]}%")
     end
