61e4aef7677a60a8af79ee756c2d01d9d72f41a9
Olivier Genel <zotri@hotmail.com>
2022-12-20 15:55:23 +0100

    Confirmed column added to events table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_30_133244) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_20_144626) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,6 +65,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_30_133244) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "price"
+    t.boolean "confirmed", default: false
     t.index ["venue_id"], name: "index_events_on_venue_id"
   end
 
