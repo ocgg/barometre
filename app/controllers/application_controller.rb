86a20ff7e97ffb089e95d9755c834fc7f76fc907
Julie Tailleur <julie.tailleur@gmail.com>
2023-01-10 18:08:08 +0100

    pundit operationnel

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   include Pundit::Authorization
 
-  after_action :verify_authorized, except: %i[index map filter], unless: :skip_pundit?
-  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+  after_action :verify_authorized, only: %i[edit update destroy], unless: :skip_pundit?
+  # after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
   def default_url_options
     { host: ENV["DOMAIN"] || "localhost:3000" }
