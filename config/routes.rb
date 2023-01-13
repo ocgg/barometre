bdd2b74d3d3244fa089c1cb86c22220d708907f5
Olivier Genel <zotri@hotmail.com>
2023-01-13 16:40:03 +0100

    Routes modifiées, fonction admin confirmer/supprimer event ou venue totalement fonctionnelle

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,10 @@ Rails.application.routes.draw do
   get '/admin', to: 'admin_pages#admin_home'
   get '/admin/events', to: 'admin_pages#admin_events'
   get '/admin/venues', to: 'admin_pages#admin_venues'
-  post 'admin/events/:id', to: 'admin_pages#event_confirm', as: :event_confirm
-  post 'admin/events/:id', to: 'admin_pages#event_reject', as: :event_reject
+  patch 'events/:id', to: 'admin_pages#event_confirm', as: :event_confirm
+  delete 'events/:id', to: 'admin_pages#event_reject', as: :event_reject
+  patch 'venues/:id', to: 'admin_pages#venue_confirm', as: :venue_confirm
+  delete 'venues/:id', to: 'admin_pages#venue_reject', as: :venue_reject
 
   get '/map', to: 'events#map', as: :map
   get '/filter', to: 'events#filter', as: :filter
