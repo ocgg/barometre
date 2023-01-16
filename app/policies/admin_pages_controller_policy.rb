c112798d205a1d7cc6fc27a1026dc17a6fa407ef
Julie Tailleur <julie.tailleur@gmail.com>
2023-01-16 17:40:57 +0100

    form edit

--- app/policies/admin_pages_controller_policy.rb
+++ app/policies/admin_pages_controller_policy.rb
@@ -3,27 +3,27 @@ class AdminPagesControllerPolicy < ApplicationPolicy
     user.admin?
   end
 
-  def admin_events?
-    user.admin?
-  end
+  # def admin_events?
+  #   user.admin?
+  # end
 
-  def admin_venues?
-    user.admin?
-  end
+  # def admin_venues?
+  #   user.admin?
+  # end
 
-  def event_confirm?
-    user.admin?
-  end
+  # def event_confirm?
+  #   user.admin?
+  # end
 
-  def event_destroy?
-    user.admin?
-  end
+  # def event_destroy?
+  #   user.admin?
+  # end
 
-  def venue_confirm?
-    user.admin?
-  end
+  # def venue_confirm?
+  #   user.admin?
+  # end
 
-  def venue_destroy?
-    user.admin?
-  end
+  # def venue_destroy?
+  #   user.admin?
+  # end
 end
