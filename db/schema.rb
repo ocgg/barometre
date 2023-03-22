6a10db3291d35b1c4e3cac457bdaf987c03ae85f
Olivier G <zotri@hotmail.com>
2023-03-22 18:38:27 +0100

    removed flats table

--- db/schema.rb
+++ db/schema.rb
@@ -71,13 +71,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_11_144724) do
     t.index ["venue_id"], name: "index_events_on_venue_id"
   end
 
-  create_table "flats", force: :cascade do |t|
-    t.string "name"
-    t.string "address"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "preferences", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "subcategory_id", null: false
