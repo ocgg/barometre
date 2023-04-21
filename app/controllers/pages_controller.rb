5cfd4dc7fefe957add2b35795936e14272ee92cd
Olivier G <zotri@hotmail.com>
2023-04-21 17:10:37 +0200

    email OK en developement

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,22 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :contact]
+  skip_before_action :authenticate_user!, only: %i[home contact send_contact_mail]
 
   def home
   end
 
   def contact
-    # @content = params[......]
-    # ContactMailer.with(content: @content).contact_mail.deliver_later
+  end
+
+  def send_contact_mail
+    @user = user_signed_in? ? current_user.email : 'anonyme'
+    @user_email = params['email']
+    @content = params['message']
+    # raise
+    ContactMailer
+      .with(user: @user,
+            user_email: @user_email,
+            content: @content)
+      .contact_mail
+      .deliver_later
   end
 end
