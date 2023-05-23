5f648aea67fe72c34130e740f1727dced1fa18a5
Julie Tailleur <julie.tailleur@gmail.com>
2023-05-23 18:14:41 +0200

    scrap bis

--- db/scraptest.rb
+++ db/scraptest.rb
@@ -1,4 +1,4 @@
-#test scrap
+# test scrap
 require "open-uri"
 require "nokogiri"
 
@@ -7,7 +7,7 @@ subcat_music = ['Concert', 'DJ Set', 'Jam Session', 'Rock', 'Rap', 'World', 'Fol
                 'Blind Test', 'Electro', 'Chanson', 'Slam', 'Karaoké', 'Jazz', 'Métal', 'Blues']
 subcats = subcat_theatre + subcat_music
 
-#ouvre lien de la page venue sur Grabuge
+# ouvre lien de la page venue sur Grabuge
 html = URI.open("https://www.grabugemag.com/lieu/lune-froide/").read
 doc = Nokogiri::HTML.parse(html)
 elements = doc.search(".tribe-events-read-more")
@@ -17,17 +17,30 @@ elements.each do |elem|
 end
 event_test = array_parse.first(1)
 
-#va ds le lien de chaque evenement de la venue
+# va ds le lien de chaque evenement de la venue pour scrapper
 event_test.each do |url|
   evt = Nokogiri::HTML.parse(URI.open("#{url}").read)
-  event_name = evt.search(".tribe-events-single-event-title").children[0].text
-  event_day = evt.search(".tribe-events-abbr.updated.published.dtstart").attribute("title").value
-  event_hour = evt.search(".updated.published.time-details").text.strip.gsub("@ ", "").gsub("h", ":")
-  event_date = DateTime.parse("#{event_day} #{event_hour}", '%Y-%m-%d %H-%M')
-  event_tags = []
-  event_tags << evt.search(".tribe-event-tags").text.downcase
-  event_tags << evt.search(".cmsmasters_cat_color").text.downcase
-  event_desc = evt.search(".tribe-events-single-event-description").children.text
-  event_photo_url = evt.search(".attachment-cmsmasters-project-grid-thumb.wp-post-image").attribute("src").value
-  
+  evt_name = evt.search(".tribe-events-single-event-title").children[0].text
+  evt_venue = evt.search(".cmsmasters_event_meta_info_item_descr.author").children.text
+  # venue = Venue.where("name LIKE ?", "%#{evt_venue}%")
+  evt_day = evt.search(".tribe-events-abbr.updated.published.dtstart").attribute("title").value
+  evt_hour = evt.search(".updated.published.time-details").text.strip.gsub("@ ", "").gsub("h", ":")
+  evt_date = DateTime.parse("#{evt_day} #{evt_hour}", '%Y-%m-%d %H-%M')
+  evt_tags = []
+  evt_tags << evt.search(".tribe-event-tags").text.downcase
+  evt_tags << evt.search(".cmsmasters_cat_color").text.downcase
+  evt_desc = evt.search(".tribe-events-single-event-description").children.text
+  evt_photo_url = evt.search(".attachment-cmsmasters-project-grid-thumb.wp-post-image").attribute("src").value
+  p evt_venue
+  # CREER L'OBJET EVENT
+  # event = Event.new(
+  #   name: evt_name,
+  #   description: evt_desc,
+  #   date: evt_date,
+  #   venue: # Equivaut à: venue: venue
+  # )
+  # ATTACHER LA PHOTO
+  # file = URI.open(evt_photo_url)
+  # event.photo.attach(io: file, filename: "#{evt_date.strftime('%Y%m%d')}-#{venue.name.gsub(' ', '')}.jpg", content_type: "image/jpg")
+  # event.confirmed = false
 end
